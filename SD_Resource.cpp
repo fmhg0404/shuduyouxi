0da85202994267ba2d85b51178794b3d2c532c34
fmhg0404 <1305808664@qq.com>
2024-12-02 21:25:44 +0800

    Add files via upload

--- SD_Resource.cpp
+++ SD_Resource.cpp
@@ -1,1 +1,66 @@
 #include "SD_Resource.h"
+
+IMAGE numimg[11], rightimg[11], button[11], choose[10], background[2];    //´æ´¢¸÷ÀàÍ¼Æ¬
+RECT r = { TIME_SHOW_X, TIME_SHOW_Y, TIME_SHOW_X + TIME_SHOW_L, TIME_SHOW_Y + TIME_SHOW_H };
+TCHAR str[30];
+SHUDU SD;
+
+void SD_import(void)
+{
+	//Êý×ÖÇøÓòÍ¼Æ¬
+	loadimage(&numimg[0], _T("./Image/0.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+	loadimage(&numimg[1], _T("./Image/1.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+	loadimage(&numimg[2], _T("./Image/2.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+	loadimage(&numimg[3], _T("./Image/3.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+	loadimage(&numimg[4], _T("./Image/4.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+	loadimage(&numimg[5], _T("./Image/5.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+	loadimage(&numimg[6], _T("./Image/6.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+	loadimage(&numimg[7], _T("./Image/7.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+	loadimage(&numimg[8], _T("./Image/8.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+	loadimage(&numimg[9], _T("./Image/9.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+	loadimage(&numimg[10], _T("./Image/10.jpg"), GAME_PICTURE_SIZE, GAME_PICTURE_SIZE);
+
+	//ÓÒ»÷ÇøÓòÍ¼Æ¬
+	loadimage(&rightimg[0], _T("./Image/0.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+	loadimage(&rightimg[1], _T("./Image/1.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+	loadimage(&rightimg[2], _T("./Image/2.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+	loadimage(&rightimg[3], _T("./Image/3.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+	loadimage(&rightimg[4], _T("./Image/4.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+	loadimage(&rightimg[5], _T("./Image/5.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+	loadimage(&rightimg[6], _T("./Image/6.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+	loadimage(&rightimg[7], _T("./Image/7.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+	loadimage(&rightimg[8], _T("./Image/8.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+	loadimage(&rightimg[9], _T("./Image/9.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+	loadimage(&rightimg[10], _T("./Image/10.jpg"), RIGHT_PICTURE_SIZE, RIGHT_PICTURE_SIZE);
+
+	//°´Å¥ÇøÓò
+	loadimage(&button[0], _T("./Image/button1.jpg"), BUTTON_MAIN_L, BUTTON_MAIN_H);
+	loadimage(&button[1], _T("./Image/button3.jpg"), BUTTON_MAIN_L, BUTTON_MAIN_H);
+	loadimage(&button[2], _T("./Image/button4.jpg"), BUTTON_MAIN_L, BUTTON_MAIN_H);
+	loadimage(&button[3], _T("./Image/button5.jpg"), BUTTON_MAIN_L, BUTTON_MAIN_H);
+	loadimage(&button[4], _T("./Image/button2.jpg"), BUTTON_MAIN_L, BUTTON_MAIN_H);
+	loadimage(&button[5], _T("./Image/button6.jpg"), BUTTON_MAIN_L, BUTTON_MAIN_H);
+	loadimage(&button[6], _T("./Image/button7.jpg"), BUTTON_MAIN_L, BUTTON_MAIN_H);
+	loadimage(&button[7], _T("./Image/mode1.jpg"), BUTTON_SET_L, BUTTON_SET_H);
+	loadimage(&button[8], _T("./Image/mode2.jpg"), BUTTON_SET_L, BUTTON_SET_H);
+	loadimage(&button[9], _T("./Image/mode3.jpg"), BUTTON_SET_L, BUTTON_SET_H);
+	loadimage(&button[10], _T("./Image/mode4.jpg"), BUTTON_SET_L, BUTTON_SET_H);
+	
+	//Ñ¡ÔñÇøÓòÍ¼Æ¬
+	loadimage(&choose[0], _T("./Image/1.jpg"), CHOOSE_PICTURE_SIZE, CHOOSE_PICTURE_SIZE);
+	loadimage(&choose[1], _T("./Image/2.jpg"), CHOOSE_PICTURE_SIZE, CHOOSE_PICTURE_SIZE);
+	loadimage(&choose[2], _T("./Image/3.jpg"), CHOOSE_PICTURE_SIZE, CHOOSE_PICTURE_SIZE);
+	loadimage(&choose[3], _T("./Image/4.jpg"), CHOOSE_PICTURE_SIZE, CHOOSE_PICTURE_SIZE);
+	loadimage(&choose[4], _T("./Image/5.jpg"), CHOOSE_PICTURE_SIZE, CHOOSE_PICTURE_SIZE);
+	loadimage(&choose[5], _T("./Image/6.jpg"), CHOOSE_PICTURE_SIZE, CHOOSE_PICTURE_SIZE);
+	loadimage(&choose[6], _T("./Image/7.jpg"), CHOOSE_PICTURE_SIZE, CHOOSE_PICTURE_SIZE);
+	loadimage(&choose[7], _T("./Image/8.jpg"), CHOOSE_PICTURE_SIZE, CHOOSE_PICTURE_SIZE);
+	loadimage(&choose[8], _T("./Image/9.jpg"), CHOOSE_PICTURE_SIZE, CHOOSE_PICTURE_SIZE);
+	loadimage(&choose[9], _T("./Image/11.jpg"), 3 * CHOOSE_PICTURE_SIZE + 2, CHOOSE_PICTURE_SIZE);
+
+	//±³¾°Í¼Æ¬
+	loadimage(&background[0], _T("./Image/background.jpg"), WINDOW_SIZE_L, WINDOW_SIZE_H);
+	loadimage(&background[1], _T("./Image/gamebackground.jpg"), GAME_BACKGROUND_SIZE, GAME_BACKGROUND_SIZE);
+
+
+}
--- SD_Resource.h
+++ SD_Resource.h
@@ -1,8 +1,14 @@
 #ifndef __SD_RESOURCE_H__
 #define __SD_RESOURCE_H__
 
+typedef unsigned char u_8;
+typedef unsigned short u_16;
+typedef unsigned int u_32;
+
 //头文件引用
 #include <graphics.h>
+#include <mmsystem.h>
+#include <time.h>
 #include "SD_Getkey.h"
 #include "SD_Init.h"
 #include "SD_Mouse.h"
@@ -12,8 +18,10 @@
 #include "SD_Set.h"
 #include "SD_Show.h"
 #include "SD_Solve.h"
+
 //关闭命令行窗口
 #pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+#pragma comment(lib,"winmm.lib")
 
 //宏定义
 #define WINDOW_SIZE_L          800//绘制窗口长度
@@ -44,13 +52,94 @@
 #define BUTTON_MAIN_H          50//主界面按钮尺寸宽
 #define BUTTON_MINTERVAL       30//主界面按钮间隔
 
-#define BUTTON_START_X         300//设置界面按钮起始位置x轴
-#define BUTTON_START_Y         150//设置界面按钮起始位置y轴
+#define BUTTON_SSTART_X         300//设置界面按钮起始位置x轴
+#define BUTTON_SSTART_Y         150//设置界面按钮起始位置y轴
 #define BUTTON_SET_L           200//设置界面按钮尺寸长
 #define BUTTON_SET_H           70//设置界面按钮尺寸宽
 #define BUTTON_SINTERVAL       30//设置界面按钮间隔
 
+
+
+//游戏功能
+typedef enum gameflag
+{
+	EMPTY,
+	GAME_START,
+	GAME_STOP,
+	GAME_AREA,
+	CHOOSE_AREA,
+	GAME_CLEAR,
+	PLAY_AGAIN,
+	GAME_HINT,
+	GAME_SET,
+	SET_EASY,
+	SET_HAND,
+	SET_HARDEST,
+	SET_SELF,
+	SET_BACK,
+	SET_TIME,
+	SET_SELF_YES
+}GAME_FLAG;
+
+//输入位置的移动
+typedef enum move
+{
+	NOT_MOVE,
+	LEFT,
+	UP,
+	RIGHT,
+	DOWN,
+}IN_MOVE;
+
+//处于哪个界面
+typedef enum Interface {
+	MAIN,
+	SET,
+	SET_USE
+}INT_FACE;
+
+//结构体定义
+typedef struct sudoku
+{
+	u_8 init[9][9] = { 0 };
+	u_8 solve[9][9] = { 0 };
+	u_8 game[9][9] = { 0 };
+	u_8 right_date[9][9][10] = { 0 };
+
+	GAME_FLAG game_flag = EMPTY;
+	IN_MOVE game_insite_move = NOT_MOVE;
+	bool Linputflag = 0;
+	bool Rinputflag = 0;
+	bool Gameing = 0;
+	bool New_state = 1;
+	INT_FACE INT_face = MAIN;
+
+	bool Mouse_flag = 0;
+
+	u_8 number = 0;
+	u_8 degree = 23;
+	u_8 degree_time = 20;
+}SHUDU;
+
+//游戏时间
+typedef struct game_time
+{
+	u_32 start = 0;
+	u_32 last = 0;
+	u_32 temp = 0;
+	u_32 total = 0;
+}GAME_TIME;
+
+//变量声明
+extern SHUDU SD;
 extern IMAGE numimg[11], rightimg[11], button[11], choose[10], background[2];    //存储各类图片
+extern BYTE LpKeyState_sd[256];
+extern ExMessage mouse_msg;
+extern HWND hwnd;
+extern RECT r;
+extern TCHAR str[30];
+extern GAME_TIME TIME;
+
 
 void SD_import(void);
 
