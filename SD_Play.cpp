c719256937b801cc79357a5f68abdebcba05e931
fmhg0404 <1305808664@qq.com>
2024-12-02 21:36:56 +0800

    Add files via upload

--- SD_Play.cpp
+++ SD_Play.cpp
@@ -1,1 +1,38 @@
 #include "SD_Play.h"
+
+//函数
+void SD_Play_game(void)
+{
+	//时间输出
+
+	//鼠标扫描
+
+	//键盘扫描
+
+	//游戏功能
+	switch (SD.game_flag)
+	{
+	case GAME_START:
+		
+	case GAME_STOP:
+		
+	case GAME_AREA:
+
+		break;
+	case CHOOSE_AREA:
+
+		break;
+	case GAME_CLEAR:
+
+		break;
+	case PLAY_AGAIN:
+
+		break;
+	case GAME_HINT:
+
+		break;
+	case GAME_SET:
+
+		break;
+	}
+}
